38318afd7c785f6e45f1cb9c83358f1276b184f5
ShangJin Li <shangli@ShangJins-MacBook-Pro.local>
2020-09-16 14:31:00 -0700

    Add Transaction.proto and GlobalConfig

---
+++ src/main/java/io/openmarket/config/GlobalConfig.java
@@ -0,0 +1,16 @@
+package io.openmarket.config;
+
+public final class GlobalConfig {
+    private GlobalConfig() {}
+
+    /**
+     * ---------------------------------------------
+     * Auth Configuration
+     * ---------------------------------------------
+
+     */
+    /**
+     * The attribute name for authentication token.
+     */
+    public static final String ATTR_AUTHENTICATION_TOKEN = "AuthToken";
+}
